2495bd9e8ae26ef5f604e7cab0720381ee4a67cd
Unknown <biigarciam@gmail.com>
2019-06-25 09:30:14 -0300

    Conserta erro url cliente

--- Locadora/server/grails-app/domain/br/ufscar/dc/dsw/Cliente.groovy
+++ Locadora/server/grails-app/domain/br/ufscar/dc/dsw/Cliente.groovy
@@ -2,7 +2,7 @@ package br.ufscar.dc.dsw
 
 import grails.rest.Resource
 
-@Resource(uri='/livros', readOnly = false, formats = ['json', 'xml'])
+@Resource(uri='/clientes', readOnly = false, formats = ['json', 'xml'])
 class Classe{
 
     static constraints = {
